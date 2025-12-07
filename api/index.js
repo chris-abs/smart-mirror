14305dec2f78a1ee87e615c6f9004703da1d2bd8
chris abbott <chris.abs96@gmail.com>
2025-12-07 21:21:37 +0000

    chore: BE - spotify scaffolding

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import spotifyRouter from "./modules/spotify/spotify.routes.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,8 @@ app.use(express.json());
 app.get("/api/health", (req, res) => {
   res.json({ ok: true, message: "API is running" });
 });
+
+app.use("/api/spotify", spotifyRouter);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
