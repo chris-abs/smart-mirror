0bcd0f4dae64c2f35cca4b3e565459a66060b087
chris abbott <chris.abs96@gmail.com>
2025-12-31 23:18:53 +0000

    feat: FE add WorkoutCanvas placeholder component

---
+++ client/src/components/organisms/workout-canvas.tsx
@@ -0,0 +1,16 @@
+export function WorkoutCanvas() {
+  return (
+    <>
+      <header className="flex justify-center items-center px-8 py-6">
+        <div className="text-4xl font-bold">Workout Mode</div>
+      </header>
+
+      <main className="flex-1 px-8 pb-8 flex flex-col items-center justify-center">
+        <div className="text-xl opacity-60">
+          Workout features coming soon...
+        </div>
+      </main>
+    </>
+  );
+}
+
