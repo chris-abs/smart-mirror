40cfb83ed1610c10c7031416fdb57a4ca6bb8f55
chris abbott <chris.abs96@gmail.com>
2025-12-31 23:19:28 +0000

    feat: FE wire up navigation system in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
-import { Canvas } from "./components/organisms/canvas";
+import { NavigationProvider, useNavigation } from "./contexts/navigation-context";
+import { Layout } from "./components/organisms/layout";
+import { HomeCanvas } from "./components/organisms/home-canvas";
+import { WorkoutCanvas } from "./components/organisms/workout-canvas";
+
+function AppContent() {
+  const { currentRoute } = useNavigation();
+
+  return (
+    <Layout>
+      {currentRoute === "home" && <HomeCanvas />}
+      {currentRoute === "workout" && <WorkoutCanvas />}
+      {currentRoute === "user" && <HomeCanvas />}
+    </Layout>
+  );
+}
 
 function App() {
-  return <Canvas />;
+  return (
+    <NavigationProvider>
+      <AppContent />
+    </NavigationProvider>
+  );
 }
 
 export default App;
