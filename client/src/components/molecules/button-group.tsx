e2887207130bf3a36bfa2580e8b352c00021136b
chris abbott <chris.abs96@gmail.com>
2025-12-31 23:10:09 +0000

    chore: FE misc cleanup

--- client/src/components/molecules/button-group.tsx
+++ client/src/components/molecules/button-group.tsx
@@ -4,15 +4,15 @@ import { IconButton } from "../atoms/icon-button";
 
 export function ButtonGroup() {
   return (
-    <div className="w-full flex justify-center items-center gap-2">
-      <IconButton aria-label="Home" className="rounded-xl px-6 py-8">
-        <Home className="size-5" />
+    <div className="w-full flex justify-center items-center gap-3">
+      <IconButton aria-label="Home" className="rounded-xl px-4 py-8">
+        <Home className="size-7" />
       </IconButton>
-      <IconButton aria-label="Fitness" className="rounded-xl px-6 py-8">
-        <Dumbbell className="size-5" />
+      <IconButton aria-label="Fitness" className="rounded-xl px-4 py-8">
+        <Dumbbell className="size-7" />
       </IconButton>
-      <IconButton aria-label="User" className="rounded-xl px-6 py-8">
-        <User className="size-5" />
+      <IconButton aria-label="User" className="rounded-xl px-4 py-8">
+        <User className="size-7" />
       </IconButton>
     </div>
   );
